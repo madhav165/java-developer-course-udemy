d9609dde8a61734ccd520be6f1762d8dc027e1fa
Madhav Kandukuri <madhav165@gmail.com>
2022-05-27 14:06:55 +0530

    area calculator

---
+++ AreaCalculator/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main (String[] args) {
+
+        System.out.println(AreaCalculator.area(1));
+        System.out.println(AreaCalculator.area(2, 3));
+
+    }
+}
