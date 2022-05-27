ea0ae3ac436f026c0b396745b62422f6af74997e
Madhav Kandukuri <madhav165@gmail.com>
2022-05-27 19:02:46 +0530

    ex 24

---
+++ exercises/Ex24_FlourPacker/src/FlourPacker.java
@@ -0,0 +1,14 @@
+public class FlourPacker {
+
+    public static boolean canPack (int bigCount, int smallCount, int goal) {
+        if ((bigCount < 0) || (smallCount < 0) || (goal < 0)) return false;
+        if ((bigCount == 0) && (goal <= 5 * bigCount + smallCount) && (goal <= smallCount)) {
+            return true;
+        } else if ((bigCount != 0) && (goal <= 5 * bigCount + smallCount) && ((goal % (5 * bigCount)) % 5 <= smallCount)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
