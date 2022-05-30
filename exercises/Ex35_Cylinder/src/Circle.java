def4e9b3b1b4ac91a1df40c937680fa1ea1390a3
Madhav Kandukuri <madhav165@gmail.com>
2022-05-30 16:35:42 +0530

    ex 35

---
+++ exercises/Ex35_Cylinder/src/Circle.java
@@ -0,0 +1,16 @@
+public class Circle {
+
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius >= 0 ? radius : 0;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public double getArea() {
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+}
